f05f611f8896a8094d477a900dcd0d3adf43a632
Max Rosin <git@hackrid.de>
2018-12-15 21:19:25 +0100

    raise version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='icinga2telegram',
-    version='0.2.0',
+    version='0.2.2',
     author='Max Rosin',
     url='https://github.com/ekeih/icinga2telegram',
     author_email='git@hackrid.de',
